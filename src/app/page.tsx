a4dde081b133d088da455e2f1563d313f8f10f3a
AhmedGamalSalem <modasalem8@gmail.com>
2026-07-26 15:25:56 +0300

    Refactor footer and remove unused image
    Updated the footer layout and removed redundant image section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         if (res.ok) {
           const mess = await res.json()
           setMes(mess.mes)
-          localStorage.setItem("nameOfUser",mes)
+          localStorage.setItem("nameOfUser", mes)
           setTimeout(() => {
             router.push(`${mes}`)
           }, 10);
@@ -41,29 +41,28 @@ export default function Home() {
         </div>
       </div>
       <Hed />
+
+      <div className='mt-4 mb-5 '>
+        <Image className='rounded-3xl w-full h-[500]' src={book} alt=''></Image>
+      </div>
       <div className='bg-indigo-600 p-3  font-bold rounded-3xl mt-5 text-l md:text-2xl '>
         Get free 100 pound  to buy books <Link href={'/login'} className='text-cyan-400'> sing now</Link>
       </div>
-      <div className='mt-4 mb-5 '>
-        <Image className='rounded-3xl w-[95vm]' src={book} alt=''></Image>
-      </div>
       <section >
-        <footer className="w-full rounded-2xl bg-[#1E293B] border-t border-slate-800 py-6 mt-auto">
-                <div className="max-w-[95%] mx-auto px-4 flex flex-col sm:flex-row items-center justify-between text-sm text-slate-400 gap-4">
-                    <div>
-                        <p>© {new Date().getFullYear()} Book App . All rights reserved.</p>
-                    </div>
-                    <div>
-                      <p>provided by Ahmed Gamal Salem 2026</p>
-                    </div>
-                    <div className="flex gap-6">
-                        {/* روابط الفوتر - عدلها براحتك */}
-
-                        <Link href="https://github.com/agsalem" target="blank" className="hover:text-amber-500/85 transition-colors">github</Link>
-                        <Link href="https://github.com/AGsalem/Book_App_Front" target="blank" className="hover:text-amber-500/85 transition-colors">Source Code Front</Link>
-                    </div>
-                </div>
-            </footer>
+        <footer className=" rounded-2xl  border-t border-slate-800  ">
+          <div className=" w-full  bg-[#1E293B] mx-auto  sm:flex-row items-center min-h-full flex flex-col p-9 justify-between text-sm text-slate-400 gap-4">
+            <div >
+              <p>© {new Date().getFullYear()} Book App . All rights reserved.</p>
+            </div>
+            <div >
+              <p>provided by Ahmed Gamal Salem 2026</p>
+            </div>
+            <div className='text-x flex gap-6'>
+              <Link href="https://github.com/agsalem" target="blank" className="hover:text-amber-500/85 transition-colors">github</Link>
+              <Link href="https://github.com/AGsalem/Book_App_Front" target="blank" className="hover:text-amber-500/85 transition-colors">Source Code Front</Link>
+            </div>
+          </div>
+        </footer>
       </section>
     </>
   );
