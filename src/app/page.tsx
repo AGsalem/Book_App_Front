d730f11c3e57c8b2e5a36d10af764d10d4b9cd74
AhmedGamalSalem <modasalem8@gmail.com>
2026-07-26 15:04:52 +0300

    Enhance Home component with image and footer
    Added an image and footer section with links to GitHub and source code.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client"
+import book from './book.png';
 import { useRouter } from 'next/navigation'
 import Hed from "@/componet/hed";
 import Link from "next/link";
+import Image from 'next/image';
 import { useState, useEffect } from "react";
 export default function Home() {
   const [err, seterr] = useState("") as any
@@ -42,9 +44,27 @@ export default function Home() {
       <div className='bg-indigo-600 p-3  font-bold rounded-3xl mt-5 text-l md:text-2xl '>
         Get free 100 pound  to buy books <Link href={'/login'} className='text-cyan-400'> sing now</Link>
       </div>
-      <div className=''>
-        {/*نا هيتحط الكارد  الي هيظهر فية الداتا  للكتب*/}
+      <div className='mt-4 mb-5 '>
+        <Image className='rounded-3xl w-[95vm]' src={book} alt=''></Image>
       </div>
+      <section >
+        <footer className="w-full rounded-2xl bg-[#1E293B] border-t border-slate-800 py-6 mt-auto">
+                <div className="max-w-[95%] mx-auto px-4 flex flex-col sm:flex-row items-center justify-between text-sm text-slate-400 gap-4">
+                    <div>
+                        <p>© {new Date().getFullYear()} Book App . All rights reserved.</p>
+                    </div>
+                    <div>
+                      <p>provided by Ahmed Gamal Salem 2026</p>
+                    </div>
+                    <div className="flex gap-6">
+                        {/* روابط الفوتر - عدلها براحتك */}
+
+                        <Link href="https://github.com/agsalem" target="blank" className="hover:text-amber-500/85 transition-colors">github</Link>
+                        <Link href="https://github.com/AGsalem/Book_App_Front" target="blank" className="hover:text-amber-500/85 transition-colors">Source Code Front</Link>
+                    </div>
+                </div>
+            </footer>
+      </section>
     </>
   );
 }
